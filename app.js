3873b4ad891257ecd54704189cee799579ca6cc9
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-01-26 19:38:24 -0800

    added some css and more html content

--- app.js
+++ app.js
@@ -96,3 +96,136 @@ var seaTac = {
 
 };
 seaTac.render();
+
+
+var seattleCenter = {
+  name: 'Seattle Center',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerCust: 6.3,
+  randCustHour: [],
+  cookieSaleHour: [],
+  totalDailyCookies: 0,
+  calcRandCustHour: function() {
+    for(var i =0; i < hours.length; i++) {
+      this.randCustHour.push(getRandomInt(this.minCust, this.maxCust));
+      //this.randCustHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
+      console.log(this.randCustHour[i]);
+    }
+  },
+  //method for cookies sold by hour
+  calcCookieSaleHour: function() {
+    for(var j = 0; j < hours.length; j++) {
+      this.cookieSaleHour.push(Math.round(this.avgCookiePerCust * this.randCustHour[j]));
+      console.log(this.cookieSaleHour[j]);
+    }
+  },
+  render: function() {
+    var seaCenter = document.getElementById('seaCenter');
+    var seaCent = document.getElementById('seaCent');
+    this.calcRandCustHour();
+    this.calcCookieSaleHour();
+    var h3El = document.createElement('h3');
+    //give text to the nex h3 element
+    h3El.textContent = this.name;
+    seaCent.appendChild(h3El);
+    for(var k = 0; k < hours.length; k++) {
+      //stepping through hours array
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[k] + ': ' + this.cookieSaleHour[k] + ' cookies';
+      console.log(liEl);
+      seaCenter.appendChild(liEl);
+
+    }
+  }
+
+};
+seattleCenter.render();
+
+var capHill = {
+  name: 'Capitol Hill',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerCust: 6.3,
+  randCustHour: [],
+  cookieSaleHour: [],
+  totalDailyCookies: 0,
+  calcRandCustHour: function() {
+    for(var i =0; i < hours.length; i++) {
+      this.randCustHour.push(getRandomInt(this.minCust, this.maxCust));
+      //this.randCustHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
+      console.log(this.randCustHour[i]);
+    }
+  },
+  //method for cookies sold by hour
+  calcCookieSaleHour: function() {
+    for(var j = 0; j < hours.length; j++) {
+      this.cookieSaleHour.push(Math.round(this.avgCookiePerCust * this.randCustHour[j]));
+      console.log(this.cookieSaleHour[j]);
+    }
+  },
+  render: function() {
+    var capitolHill = document.getElementById('capitolHill');
+    var capHill = document.getElementById('capHill');
+    this.calcRandCustHour();
+    this.calcCookieSaleHour();
+    var h3El = document.createElement('h3');
+    //give text to the nex h3 element
+    h3El.textContent = this.name;
+    capHill.appendChild(h3El);
+    for(var k = 0; k < hours.length; k++) {
+      //stepping through hours array
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[k] + ': ' + this.cookieSaleHour[k] + ' cookies';
+      console.log(liEl);
+      capitolHill.appendChild(liEl);
+
+    }
+  }
+
+};
+capHill.render();
+
+var alki = {
+  name: 'Alki',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerCust: 6.3,
+  randCustHour: [],
+  cookieSaleHour: [],
+  totalDailyCookies: 0,
+  calcRandCustHour: function() {
+    for(var i =0; i < hours.length; i++) {
+      this.randCustHour.push(getRandomInt(this.minCust, this.maxCust));
+      //this.randCustHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
+      console.log(this.randCustHour[i]);
+    }
+  },
+  //method for cookies sold by hour
+  calcCookieSaleHour: function() {
+    for(var j = 0; j < hours.length; j++) {
+      this.cookieSaleHour.push(Math.round(this.avgCookiePerCust * this.randCustHour[j]));
+      console.log(this.cookieSaleHour[j]);
+    }
+  },
+  render: function() {
+    var ulAlki = document.getElementById('ulAlki');
+    var alki = document.getElementById('alki');
+    this.calcRandCustHour();
+    this.calcCookieSaleHour();
+    var h3El = document.createElement('h3');
+    //give text to the nex h3 element
+    h3El.textContent = this.name;
+    alki.appendChild(h3El);
+    for(var k = 0; k < hours.length; k++) {
+      //stepping through hours array
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[k] + ': ' + this.cookieSaleHour[k] + ' cookies';
+      console.log(liEl);
+      ulAlki.appendChild(liEl);
+
+    }
+  }
+
+};
+alki.render();
