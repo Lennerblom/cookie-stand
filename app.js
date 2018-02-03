adc2653de26598089ee381a306791dfba550423a
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-02-03 15:32:19 -0800

    table totals a bit off from what I want and I can't delete the previous table yet

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ var seaTac =new CookieStore('Seatac Airport', 3, 24, 1.2);
 var seaCent = new CookieStore('Seattle Center', 11, 38, 3.7);
 var capHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
 var alki = new CookieStore('Alki', 2, 16, 4.6);
-
+//MAIN TABLE HEAD
 function makeTableHead() {
   var cookieTable = document.getElementById('cookieTable');
   var trEl = document.createElement('tr');
@@ -74,7 +74,7 @@ function makeTableHead() {
 }
 
 makeTableHead();
-
+//MAIN TABLE DATA ROWS
 function makeTableRow1(){
 
   var cookieTable = document.getElementById('cookieTable');
@@ -82,9 +82,9 @@ function makeTableRow1(){
   var tdEl = document.createElement('td');
   for (var i = 0; i < storeLocations.length; i++){
     trEl = document.createElement('tr');
+    trEl.setAttribute('id', 'mainTableDataRows');
     tdEl.textContent = storeLocations[i].name;
     trEl.appendChild(tdEl);
-    console.log(trEl.appendChild(tdEl));
 
     for(var j = 0; j < hours.length; j++) {
       tdEl = document.createElement('td');
@@ -103,40 +103,35 @@ function makeTableRow1(){
 }
 
 makeTableRow1();
-
-function makeTableTotal(){
+//TOTALS BY HOUR ROW
+function makeTableHourlyTotal(){
   var cookieTable = document.getElementById('cookieTable');
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
   var tdEl = document.createElement('td');
-  //var total = storeLocations[i].totalDailyCookies;
-  trEl = document.createElement('tr');
   thEl.textContent = 'total';
   trEl.appendChild(thEl);
+
   var hoursTotal = 0;
-  for (var j = 0; j < hours.length; j++){
+  for (var i = 0; i < hours.length; i++){
     tdEl = document.createElement('td');
-    for (var i = 0; i < storeLocations.length; i++){
-      hoursTotal += storeLocations[i].cookieSaleHour[j];
+    for (var j = 0; j < storeLocations.length; j++){
+      hoursTotal += storeLocations[j].cookieSaleHour[i];
     }
     tdEl.textContent = hoursTotal;
     trEl.appendChild(tdEl);
     cookieTable.appendChild(trEl);
-    console.log(trEl.appendChild(tdEl));
-
   }
-}
-makeTableTotal();
 
-/*function newStoreLocation() {                             // Declare function
-  var addNewStore = document.getElementById('newStore');     // Get feedback element
-
+  tdEl = document.createElement('td');
+  totalTotal += hoursTotal;
+  tdEl.textContent = totalTotal;
+  trEl.appendChild(tdEl);
+  cookieTable.appendChild(trEl);
+  console.log(trEl.appendChild(tdEl));
 }
-  
-var elUsername = document.getElementById('username');  // Get username input
-// When it loses focus call checkUsername()
-elUsername.addEventListener('blur', checkUsername, false);*/
-//this function is the event handler for the submission of a new store
+makeTableHourlyTotal();
+
 var newStoreForm = document.getElementById('newStoreForm');
 
 function handleStoreAdd(event) {
@@ -156,18 +151,39 @@ function handleStoreAdd(event) {
     return alert('You must enter a value; please do not leave blank.');
   }
   //target the name in the form
-  
+
 
   var newStore = new CookieStore(newStoreName, newStoreMinCust, newStoreMaxCust, newStoreAvgCookie);console.log(newStore);
 
+  
+
+  function deleteTable() {
+    var cookieTable = document.getElementById('cookieTable');
+    for(var i = cookieTable.rows.length; i<0; i--) {
+      cookieTable.deleteRow(i-1);
+    }
+  }
+  deleteTable();
+  //   function removeTable() {
+  //     var cookieTable = document.getElementById('cookieTable');
+  //     var i = 6;
+  //     for(i = 6; i<0; i--) {
+  //       //cookieTable.deleterow(-1);
+  //       cookieTable.delete();
+  //     }
+  //     /* var cookieTable = document.getElementById('cookieTable');
+  //     var trEl = document.getElementById('mainTableDataRows');
+  //     cookieTable.removeChild(trEl);*/
+  //   }
+  //   removeTable();
 
   makeTableRow1();
+  makeTableHourlyTotal();
 
   event.target.storeName.value = null;
   event.target.minCust.value = null;
   event.target.maxCust.value = null;
   event.target.avgCookie.value = null;
-
 
 }
 newStoreForm.addEventListener('submit', handleStoreAdd);
